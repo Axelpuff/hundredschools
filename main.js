44e015602bbe0997799a5855fe8947572217b6d4
Axel Soderquist <asoderquist@college.harvard.edu>
2025-11-24 20:51:46 -0500

    added guqin and skybox

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const sizes = {
     width: window.innerWidth,
@@ -18,13 +19,31 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const controls = new OrbitControls(camera, canvas);
 controls.update();
 
-const geometry = new THREE.BoxGeometry(4,1,1);
+const envLoader = new THREE.TextureLoader();
+const texture = await envLoader.loadAsync('assets/images/footprint_court.jpg');
+texture.mapping = THREE.EquirectangularReflectionMapping;
+scene.environment = texture;
+
+// const ambient = new THREE.AmbientLight(0x404040);
+// scene.add(ambient);
+const directional = new THREE.DirectionalLight(0xFFFFFF, 0.5);
+directional.position.set(0, 20, 0);
+directional.target.position.set(-5, 0, 0);
+scene.add(directional);
+scene.add(directional.target);
+
+const loader = new GLTFLoader();
+const guqin = await loader.loadAsync('assets/chinese_zither/scene.gltf');
+scene.add(guqin.scene);
+
+/* const geometry = new THREE.BoxGeometry(4,1,1);
 const material = new THREE.MeshBasicMaterial({color: 0x0000ff});
 const cube = new THREE.Mesh(geometry, material);
 
-scene.add(cube);
+scene.add(cube); */
 
-camera.position.z = 3;
+camera.position.y = 60;
+//camera.rotation.x = 0;
 
 function handleResize() {
     sizes.width = window.innerWidth;
@@ -40,8 +59,8 @@ window.addEventListener("resize", handleResize);
 
 function animate() {
     renderer.render(scene, camera);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    /* cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01; */
 }
 
 renderer.setAnimationLoop(animate);
